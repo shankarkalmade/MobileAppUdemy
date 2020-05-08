d441a731b3733898b6e76b1c83c558356ae5a1ae
Shankar <shankar.kalmade@gmail.com>
2020-05-08 18:22:15 +0530

    Initial commit

---
+++ src/main/java/com/shankar/udemy/mobileapp/MobileAppUdemyApplication.java
@@ -0,0 +1,13 @@
+package com.shankar.udemy.mobileapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileAppUdemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileAppUdemyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shankar/udemy/mobileapp/MobileAppUdemyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shankar.udemy.mobileapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileAppUdemyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
